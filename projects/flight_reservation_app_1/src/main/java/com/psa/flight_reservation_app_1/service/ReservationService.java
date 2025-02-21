1504f79a42e6666e7bc5709dbff34e8516cfc310
RahulMaurya382807 <rahulmaurya.khd@gmail.com>
2025-02-21 13:40:10 +0530

    saved

---
+++ projects/flight_reservation_app_1/src/main/java/com/psa/flight_reservation_app_1/service/ReservationService.java
@@ -0,0 +1,8 @@
+package com.psa.flight_reservation_app_1.service;
+
+import com.psa.flight_reservation_app_1.dto.ReservationRequest;
+import com.psa.flight_reservation_app_1.entity.Reservation;
+
+public interface ReservationService {
+	Reservation bookFlight(ReservationRequest request);
+}
