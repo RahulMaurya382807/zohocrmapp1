8e25d0679535b4fd88dc11c3f1563135de6b6f68
RahulMaurya382807 <122665871+RahulMaurya382807@users.noreply.github.com>
2025-02-21 13:54:50 +0530

    Delete er-diagram-feb/mappingfeb directory
    delete

--- er-diagram-feb/mappingfeb/src/main/java/com/example/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
